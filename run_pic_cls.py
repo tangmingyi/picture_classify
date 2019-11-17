537febdbda11c110bb2613920282076fd96c7d48
tmy <zb19851222@qq.com>
2019-11-17 08:23:28 +0800

    1.creato_model function add resnet 2.all summary in train_summary_lt and val_summary_lt 3.use tf.unstack() and tf.image.decode_jpeg process a bytes data to picture matrix 4.add beholder summary. 5.predict result add softmax key for what-if
    note:serving sign input for predict is examples.

--- run_pic_cls.py
+++ run_pic_cls.py
@@ -16,9 +16,13 @@
 import json
 import os
 from lib.se_resnet_xt.moding import SE_ResNet_Xt
+from lib.ResNet.model import Cifar10Model
 from lib.se_resnet_xt import optimization, myhook
+from lib.se_resnet_xt.myhook import BeholderHook
 from utility.data_tool import process
 import tensorflow as tf
+import numpy as np
+# from tensorboard.plugins.beholder import BeholderHook
 
 
 from tensorflow.python import debug as tfdbg
@@ -98,11 +102,15 @@ def input_fn_builder(input_file, is_training, drop_remainder, batch):
     return input_fn
 
 
-def create_model(input, layers, class_dim):
+def create_model(input, layers, class_dim,is_train):
     """Creates a classification model."""
-    model = SE_ResNet_Xt(input, layers, class_dim)
-    cls_layers = model.get_cls_layer()
-    return cls_layers
+    # model = SE_ResNet_Xt(input, layers, class_dim)
+    # cls_layers = model.get_cls_layer()
+    activate_lt = None
+    model = Cifar10Model(8)
+    cls_layers = model(input,True)
+    activate_lt = model.get_activate_lt()
+    return cls_layers,activate_lt
 
 
 def model_fn_builder(lays, class_dim, learning_rate,
@@ -111,32 +119,61 @@ def model_fn_builder(lays, class_dim, learning_rate,
     """Returns `model_fn` closure for TPUEstimator."""
 
     def model_fn(features, labels, mode, params):  # pylint: disable=unused-argument
-
+        train_summary_lt = []
+        val_summary_lt = []
         tf.logging.info("*** Features ***")
         for name in sorted(features.keys()):
             tf.logging.info("  name = %s, shape = %s" % (name, features[name].shape))
 
         unids = features["unid"]
         picture = features["image/encoded"]
-        picture = tf.decode_raw(picture, tf.uint8)
-        picture = tf.cast(picture,tf.float32)
-        picture = tf.reshape(picture, [-1, 3, 32, 32])
-        picture = tf.transpose(picture, [0, 2, 3, 1])
 
         if mode == tf.estimator.ModeKeys.TRAIN:
-            pic_summary1 = tf.summary.image("picture_org",picture)
+            batch = params["train_batch_size"]
+        elif mode == tf.estimator.ModeKeys.EVAL:
+            batch = params["eval_batch_size"]
+        else:
+            batch = params["predict_batch_size"]
 
-        picture = tf.divide(picture, tf.constant(255, tf.float32))
+        # def body(i,input_img,out_img):
+        #     image = tf.expand_dims(tf.image.decode_jpeg(input_img[i],channels=3),0)
+        #     out_img = tf.cond(tf.equal(i,0),lambda :image,lambda :tf.concat([out_img,image],0))
+        #     return i+1,input_img,out_img
+        # i = tf.constant(0)
+        # out_pic = tf.zeros([1,32,32,3],dtype=tf.uint8)
+        # loopout = tf.while_loop(lambda i,picture,out_pic:tf.less(i,batch),body,[i,picture,out_pic],shape_invariants=[
+        #     i.get_shape(),
+        #     picture.get_shape(),
+        #     tf.TensorShape(None)
+        # ])
+        # picture = loopout[2]
+        picture_lt = tf.unstack(picture,num=batch)
+        for i in range(len(picture_lt)):
+            picture_lt[i] = tf.image.decode_jpeg(picture_lt[i],channels=3)
+        picture = tf.stack(picture_lt)
+
+        # picture = tf.decode_raw(picture, tf.uint8,name="raw_pic")
+        # picture = tf.reshape(picture, [-1, 3, 32, 32])
+        # picture = tf.transpose(picture, [0, 2, 3, 1])
+
+
+        picture = tf.cast(picture,tf.float32)
+
+
+
+
+        # picture = tf.divide(picture, tf.constant(255, tf.float32))
 
 
         #图像增强
         if mode == tf.estimator.ModeKeys.TRAIN:
-            normal_pic = tf.summary.image("normal_pic",picture)
+            pic_summary1 = tf.summary.image("picture_org",picture)
+            train_summary_lt.append(pic_summary1)
             # picture = tf.image.image_gradients
             # stand_pic_summary = tf.summary.image("stand_pic",picture)
 
-            picture = picture + 5.0*tf.random_normal(shape=[32,32,3],mean=0,stddev=0.1)
-            nosiy_pic_summary = tf.summary.image("nosiy_pic",picture)
+            # picture = picture + 5.0*tf.random_normal(shape=[32,32,3],mean=0,stddev=0.1)
+            # nosiy_pic_summary = tf.summary.image("nosiy_pic",picture)
 
             picture = tf.image.random_flip_left_right(picture)
             picture = tf.image.random_flip_up_down(picture)
@@ -145,14 +182,26 @@ def model_fn_builder(lays, class_dim, learning_rate,
             # picture = tf.image.random_brightness(picture,configDir["bright_max_delta"])
             # picture = tf.image.random_contrast(picture,configDir["contrast_lower"],configDir["contrast_up"])
 
-            pic_summary2 = tf.summary.image("picture_enhancement",picture)
-
+            # picture = tf.image.resize_image_with_crop_or_pad(
+            #     picture, 32 + 8, 32 + 8)
+            #
+            # # Randomly crop a [_HEIGHT, _WIDTH] section of the image.
+            # picture = tf.random_crop(picture, [-1,32, 32, 3])
+            #
+            # # Randomly flip the image horizontally.
+            # picture = tf.image.random_flip_left_right(picture)
+            #
+            # # Subtract off the mean and divide by the variance of the pixels.
+            # picture = tf.map_fn(tf.image.per_image_standardization,picture)
+            #
+            # pic_summary2 = tf.summary.image("picture_enhancement",picture)
+            # train_summary_lt.append(pic_summary2)
 
             # summary_hook2 = tf.train.SummarySaverHook(save_steps=configDir["save_summary_steps"],output_dir=configDir["model_dir"],summary_op=pic_summary2)
 
         label = features["label"]
 
-        cls = create_model(picture, lays, class_dim)
+        cls,activate_lt = create_model(picture, lays, class_dim,mode == tf.estimator.ModeKeys.TRAIN)
 
         tvars = tf.trainable_variables()
 
@@ -172,13 +221,18 @@ def model_fn_builder(lays, class_dim, learning_rate,
             one_hot_positions = tf.one_hot(
                 positions, depth=class_dim, dtype=tf.float32)
             log_probs = tf.nn.log_softmax(logits, axis=-1)
+            probility = tf.reduce_sum(one_hot_positions*tf.nn.softmax(logits,axis=-1),axis=-1)
             loss = tf.reduce_sum(one_hot_positions * log_probs, axis=-1)
-            return loss
+            return loss,probility
 
-        per_example_loss = compute_loss(cls, label)
+        per_example_loss,per_example_probility = compute_loss(cls, label)
         global_step = tf.train.get_or_create_global_step()
 
         if mode == tf.estimator.ModeKeys.TRAIN:
+            per_example_loss_summary = tf.summary.histogram("per_example_loss",per_example_loss)
+            per_example_probility_summary = tf.summary.histogram("per_example_pribility",per_example_probility)
+            train_summary_lt.append(per_example_loss_summary)
+            train_summary_lt.append(per_example_probility_summary)
             total_loss = -tf.reduce_mean(per_example_loss, axis=-1)
             # total_loss = tf.losses.softmax_cross_entropy(tf.one_hot(label,depth=class_dim),cls)
 
@@ -189,8 +243,8 @@ def model_fn_builder(lays, class_dim, learning_rate,
             # decay_learning_rate = tf.train.exponential_decay(learning_rate=learning_rate,global_step=global_step,decay_steps=configDir["decay_steps"],decay_rate=configDir["decay_rate"])
             # learning_rate_summary = tf.summary.scalar("decay_learning_rate",decay_learning_rate)
             # train_op = tf.train.GradientDescentOptimizer(decay_learning_rate).minimize(total_loss,global_step=global_step)
-            summary_hook1 = tf.train.SummarySaverHook(save_steps=configDir["save_summary_steps"],output_dir=configDir["model_dir"]
-                                                      ,summary_op=[pic_summary1,pic_summary2,normal_pic,nosiy_pic_summary])
+            summary_hook = tf.train.SummarySaverHook(save_steps=configDir["save_summary_steps"],output_dir=configDir["model_dir"]
+                                                      ,summary_op=train_summary_lt)
 
             output_spec = tf.estimator.EstimatorSpec(
                 mode=mode,
@@ -198,14 +252,13 @@ def model_fn_builder(lays, class_dim, learning_rate,
                 train_op=train_op,
                 export_outputs=None,
                 training_chief_hooks=None,
-                training_hooks=[summary_hook1]
+                training_hooks=[summary_hook]
                 # evalute_hook(handle=handle,feed_handle=test_handle, run_op=total_loss, evl_step=10),
                 # train_hook(handle,train_handle)
                 # ],
 
             )
         elif mode == tf.estimator.ModeKeys.EVAL:
-
             def metric_fn(per_example_loss, label, logits):
                 predictions = tf.argmax(logits, axis=-1, output_type=tf.int32)
                 accuracy = tf.metrics.accuracy(
@@ -215,12 +268,21 @@ def model_fn_builder(lays, class_dim, learning_rate,
                     "eval_accuracy": accuracy,
                     # "eval_loss": loss,
                 }
-
+            beholder_hook = BeholderHook(configDir["model_dir"],[picture]+activate_lt,np.ndarray([32,32,3]))
+            pic_summary = tf.summary.image("val_picture_org",picture)
+            per_example_loss_summary = tf.summary.histogram("val_per_example_loss",per_example_loss)
+            per_example_probility_summary = tf.summary.histogram("val_per_example_pribility",per_example_probility)
+            val_summary_lt.append(pic_summary)
+            val_summary_lt.append(per_example_probility_summary)
+            val_summary_lt.append(per_example_loss_summary)
             output_spec = tf.estimator.EstimatorSpec(
                 mode=mode,
                 loss=-tf.reduce_mean(per_example_loss, axis=-1),
                 eval_metric_ops=metric_fn(per_example_loss, label, cls),
-                scaffold=scaffold_fn)
+                scaffold=scaffold_fn,
+                evaluation_hooks=[tf.train.SummarySaverHook(save_steps=configDir["save_summary_steps"],output_dir=configDir["model_dir"]
+                                                           ,summary_op=val_summary_lt),beholder_hook]
+            )
 
         elif mode == tf.estimator.ModeKeys.PREDICT:
             predictions = {
@@ -228,6 +290,7 @@ def model_fn_builder(lays, class_dim, learning_rate,
                 "predict": tf.arg_max(cls, -1),
                 "logits":tf.arg_max(tf.nn.softmax(cls,-1),-1),
                 "label": label,
+                "softmax":tf.nn.softmax(cls,-1)
             }
             output_spec = tf.estimator.EstimatorSpec(
                 mode=mode, predictions=predictions, scaffold=scaffold_fn)
@@ -289,20 +352,20 @@ def main(_):
         return 0
 
     if configDir["do_train"] == 1:
-        input_files = [os.path.join(configDir["DP"], "train", name) for name in
-                       os.listdir(os.path.join(configDir["DP"], "train"))]
+        input_files = [os.path.join(configDir["train_input"], name) for name in
+                       os.listdir(configDir["train_input"])]
         train_input_fn = file_based_input_fn_builder(input_file=input_files, is_training=True, drop_remainder=True,
                                                      batch="train_batch_size")
 
-        input_files = [os.path.join(configDir["DP"], "test", name) for name in
-                       os.listdir(os.path.join(configDir["DP"], "test"))]
+        input_files = [os.path.join(configDir["val_input"], name) for name in
+                       os.listdir(configDir["val_input"])]
         # input_files = os.listdir(os.path.join(configDir["DP"], "test"))
-        val_input_fn = file_based_input_fn_builder(input_file=input_files, is_training=False, drop_remainder=False,
-                                                   batch="predict_batch_size")
-
+        val_input_fn = file_based_input_fn_builder(input_file=input_files, is_training=False, drop_remainder=True,
+                                                   batch="eval_batch_size")
+        trainSpecHook = [tfdbg.TensorBoardDebugHook(grpc_debug_server_addresses="localhost:11111")]
         trainSpec = tf.estimator.TrainSpec(input_fn=train_input_fn, max_steps=num_train_steps, hooks=None)
         valSpec = tf.estimator.EvalSpec(input_fn=val_input_fn, steps=configDir["trainStepVal"],
-                                        throttle_secs=configDir["throttle_secs"])
+                                        throttle_secs=configDir["throttle_secs"],hooks=None)
         tf.estimator.train_and_evaluate(estimator=estimator, train_spec=trainSpec, eval_spec=valSpec)
         # estimator.train(input_fn=train_input_fn, max_steps=num_train_steps)
         # ,hooks=[tfdbg.TensorBoardDebugHook(grpc_debug_server_addresses="localhost:11111"),
@@ -313,8 +376,8 @@ def main(_):
         tf.logging.info("***** Running predictions *****")
         tf.logging.info("  Batch size = %d", configDir["test_batch_size"])
 
-        input_files = [os.path.join(configDir["DP"], "test", name) for name in
-                       os.listdir(os.path.join(configDir["DP"], "test"))]
+        input_files = [os.path.join(configDir["predict_input"], name) for name in
+                       os.listdir(configDir["predict_input"])]
         # input_files = os.listdir(os.path.join(configDir["DP"], "test"))
         predict_input_fn = file_based_input_fn_builder(input_file=input_files, is_training=False, drop_remainder=False,
                                                        batch="predict_batch_size")
@@ -322,7 +385,10 @@ def main(_):
         wf = open(configDir["test_res_output"], "w", encoding="utf-8")
         for mm, result in enumerate(estimator.predict(
                 predict_input_fn, yield_single_examples=True
-                # ,hooks=[tf_debug.LocalCLIDebugHook(ui_type="readline")]
+                ,hooks=[
+                        # tfdbg.LocalCLIDebugHook(),
+                        # tfdbg.TensorBoardDebugHook(grpc_debug_server_addresses="localhost:11111"),
+                ]
         )):
             # if len(all_results) % 1000 == 0:
             tf.logging.info("Processing example: %d" % (mm))
