9b3263c0384fce1a8d1ab4f2eb02a30b80c3f8a8
tmy_notebook <zb19851222@qq.com>
2020-01-04 21:35:46 +0800

    将 estimate 中的 input model_build 从 run_pic_cls.py中解耦出来

--- run_pic_cls.py
+++ run_pic_cls.py
@@ -4,17 +4,12 @@ doing: 卷积特征图可视化
 """
 import json
 import os
-from lib.ResNet.model import Model
-from lib.ResNet import optimization,myhook,Loss
-from lib.ResNet.myhook import BeholderHook
-# from utility.data_tool import process
+from utility.Estimate_builder import model_input,model_build
 import tensorflow as tf
 import numpy as np
 # from tensorboard.plugins.beholder import BeholderHook
 import cv2
 import matplotlib.pyplot as plt
-
-
 from tensorflow.python import debug as tfdbg
 
 configDir = json.load(open("config_file/config.json", "r", encoding="utf-8"))
@@ -27,296 +22,21 @@ if not configDir["do_test"]:
     configDir["do_save_conv_image"] = False
 
 
-def file_based_input_fn_builder(input_file, is_training, drop_remainder, batch):
-    """Creates an `input_fn` closure to be passed to TPUEstimator."""
-    input_file = [os.path.join(input_file, name) for name in os.listdir(input_file)]
-    name_to_features = {
-        "unid": tf.FixedLenFeature([], tf.int64),
-        "image/encoded": tf.FixedLenFeature([], tf.string),
-        "label": tf.FixedLenFeature([], tf.int64),
-        "name": tf.FixedLenFeature([],tf.string)
-    }
-
-    def _decode_record(record, name_to_features):
-        """Decodes a record to a TensorFlow example."""
-        example = tf.parse_single_example(record, name_to_features)  # record是example的序列化，通过这个函数解析为features字典
-        return example
-
-    def input_fn(params):
-        """The actual input function."""
-        batch_size = params[batch]
-
-        # For training, we want a lot of parallel reading and shuffling.
-        # For eval, we want no shuffling and parallel reading doesn't matter.
-        d = tf.data.TFRecordDataset(input_file)  # 建立dataset数据的来源
-        if is_training:
-            d = d.repeat()
-            d = d.shuffle(buffer_size=100)
-
-        d = d.apply(
-            tf.contrib.data.map_and_batch(
-                lambda record: _decode_record(record, name_to_features),
-                # 对每个元素应用map函数进行张量的预处理；dataset可能会将读取的record原始序列张量，传入其中
-                batch_size=batch_size,
-                drop_remainder=drop_remainder))
-
-        return d
-
-    return input_fn
+def show_cam_on_image(img, mask):
+    heatmap = cv2.applyColorMap(np.uint8(255 * mask), cv2.COLORMAP_JET)
+    heatmap = np.float32(heatmap) / 255
+    cam_out = heatmap + np.float32(img)
+    cam_out = cam_out / np.max(cam_out)
+    plt.imshow(cam_out)
+    plt.show()
 
 
-def input_fn_builder(input_file, is_training, drop_remainder, batch):
-    """Creates an `input_fn` closure to be passed to TPUEstimator."""
-
-    name_to_features = {
-        "unid": tf.FixedLenFeature([], tf.int64),
-        "picture": tf.FixedLenFeature([], tf.string),
-        "label": tf.FixedLenFeature([], tf.int64),
-    }
-
-    def _decode_record(record, name_to_features):
-        """Decodes a record to a TensorFlow example."""
-        example = tf.parse_single_example(record, name_to_features)
-        return example
-
-    def input_fn(params):
-        """The actual input function."""
-        batch_size = params[batch]
-
-        # For training, we want a lot of parallel reading and shuffling.
-        # For eval, we want no shuffling and parallel reading doesn't matter.
-        d = tf.data.TFRecordDataset(input_file)
-        if is_training:
-            d = d.repeat()
-            d = d.shuffle(buffer_size=100)
-
-        d = d.apply(
-            tf.contrib.data.map_and_batch(
-                lambda record: _decode_record(record, name_to_features),
-                batch_size=batch_size,
-                drop_remainder=drop_remainder))
-        return d
-
-    return input_fn
-
-
-def create_model(input,is_train):
-    """Creates a classification model."""
-    activate_dict = None
-    if model_config["model_name"] == "lib/ResNet":
-        model = Model(resnet_size=model_config["resnet_size"], bottleneck=model_config["bottleneck"],
-                      num_classes=model_config["num_classes"], num_filters=model_config["num_filters"],
-                      kernel_size=model_config["kernel_size"], conv_stride=model_config["conv_stride"],
-                      first_pool_size=model_config["first_pool_size"], first_pool_stride=model_config["first_pool_stride"],
-                      block_sizes=model_config["block_sizes"], block_strides=model_config["block_strides"],
-                      final_size=model_config["final_size"], resnet_version=model_config["resnet_version"],
-                      data_format=model_config["data_format"])
-    else:
-        model = Model(resnet_size=model_config["resnet_size"], bottleneck=model_config["bottleneck"],
-                      num_classes=model_config["num_classes"], num_filters=model_config["num_filters"],
-                      kernel_size=model_config["kernel_size"], conv_stride=model_config["conv_stride"],
-                      first_pool_size=model_config["first_pool_size"], first_pool_stride=model_config["first_pool_stride"],
-                      block_sizes=model_config["block_sizes"], block_strides=model_config["block_strides"],
-                      final_size=model_config["final_size"], resnet_version=model_config["resnet_version"],
-                      data_format=model_config["data_format"])
-    cls_layers = model(input, True)
-    activate_dict = model.get_activate_dict()
-    return cls_layers, activate_dict
-
-
-def model_fn_builder(learning_rate,
-                     num_train_steps, num_warmup_steps, use_tpu=False
-                     ):
-    """Returns `model_fn` closure for TPUEstimator."""
-
-    def model_fn(features, labels, mode, params):  # pylint: disable=unused-argument
-        train_summary_lt = []
-        val_summary_lt = []
-        tf.logging.info("*** Features ***")
-        for name in sorted(features.keys()):
-            tf.logging.info("  name = %s, shape = %s" % (name, features[name].shape))
-
-        unids = features["unid"]
-        picture = features["image/encoded"]
-
-        if mode == tf.estimator.ModeKeys.TRAIN:
-            batch = params["train_batch_size"]
-        elif mode == tf.estimator.ModeKeys.EVAL:
-            batch = params["eval_batch_size"]
-        else:
-            batch = params["predict_batch_size"]
-
-
-        picture_lt = tf.unstack(picture, num=batch)
-        if configDir["grad_cam"]:
-            organ_image = []
-        for i in range(len(picture_lt)):
-            image = tf.image.decode_jpeg(picture_lt[i], channels=3)
-            image = tf.image.resize_images(image,[configDir["resize"],configDir["resize"]])
-            if configDir["grad_cam"]:
-                organ_image.append(image)
-            picture_lt[i] = tf.image.per_image_standardization(image)
-        picture = tf.stack(picture_lt)
-        if configDir["grad_cam"]:
-            organ_image = tf.stack(organ_image)
-
-
-        picture = tf.cast(picture, tf.float32)
-
-
-        # 图像增强
-        if mode == tf.estimator.ModeKeys.TRAIN:
-            pic_summary1 = tf.summary.image("picture_org", picture)
-            train_summary_lt.append(pic_summary1)
-
-
-            picture = tf.image.random_flip_left_right(picture)
-            picture = tf.image.random_flip_up_down(picture)
-
-        label = features["label"]
-
-        cls, activate_dict = create_model(picture,mode == tf.estimator.ModeKeys.TRAIN)
-
-        tvars = tf.trainable_variables()
-
-        initialized_variable_names = {}
-        scaffold_fn = None
-        tf.logging.info("**** Trainable Variables ****")
-        for var in tvars:
-            init_string = ""
-            if var.name in initialized_variable_names:
-                init_string = ", *INIT_FROM_CKPT*"
-            tf.logging.info("  name = %s, shape = %s%s", var.name, var.shape,
-                            init_string)
-
-        output_spec = None
-
-
-        per_example_loss, per_example_probility = Loss.prob_CEloss(cls, label,model_config["num_classes"])
-        global_step = tf.train.get_or_create_global_step()
-
-        if mode == tf.estimator.ModeKeys.TRAIN:
-            train_hook_lt = []
-            per_example_loss_summary = tf.summary.histogram("per_example_loss", -per_example_loss)
-            per_example_probility_summary = tf.summary.histogram("per_example_pribility", per_example_probility)
-            train_summary_lt.append(per_example_loss_summary)
-            train_summary_lt.append(per_example_probility_summary)
-            if configDir["loss_name"] == "GHM":
-                loss_config = configDir["GHM_loss_config"]
-                total_loss,weight = Loss.GHMLoss(-per_example_loss,loss_config["bin"],loss_config["step_length"],batch)
-                train_summary_lt.append(tf.summary.histogram("loss_weight",weight))
-            elif configDir["loss_name"] == "focal":
-                loss_config = configDir["focal_loss_config"]
-                total_loss,weight = Loss.focal_loss1(tf.one_hot(label,depth=model_config["num_classes"]),cls,loss_config["gamma"])
-                train_summary_lt.append(tf.summary.histogram("loss_weight",weight))
-            else:
-                total_loss = -tf.reduce_mean(per_example_loss, axis=-1)
-            # total_loss = tf.losses.softmax_cross_entropy(tf.one_hot(label,depth=class_dim),cls)
-
-            train_op = optimization.create_optimizer(
-                total_loss, learning_rate, num_train_steps, num_warmup_steps, use_tpu, -1)
-            # train_op = tf.train.AdagradDAOptimizer(learning_rate=configDir["learning_rate"]
-            #                                        ,global_step=global_step).minimize(total_loss,global_step=global_step)
-            # decay_learning_rate = tf.train.exponential_decay(learning_rate=learning_rate,global_step=global_step,decay_steps=configDir["decay_steps"],decay_rate=configDir["decay_rate"])
-            # learning_rate_summary = tf.summary.scalar("decay_learning_rate",decay_learning_rate)
-            # train_op = tf.train.GradientDescentOptimizer(decay_learning_rate).minimize(total_loss,global_step=global_step)
-            summary_hook = tf.train.SummarySaverHook(save_steps=configDir["save_summary_steps"],
-                                                     output_dir=configDir["model_dir"]
-                                                     , summary_op=train_summary_lt)
-            train_hook_lt.append(summary_hook)
-
-            output_spec = tf.estimator.EstimatorSpec(
-                mode=mode,
-                loss= -tf.reduce_mean(per_example_loss, axis=-1),
-                train_op=train_op,
-                export_outputs=None,
-                training_chief_hooks=None,
-                training_hooks=train_hook_lt
-                # evalute_hook(handle=handle,feed_handle=test_handle, run_op=total_loss, evl_step=10),
-                # train_hook(handle,train_handle)
-                # ],
-
-            )
-        elif mode == tf.estimator.ModeKeys.EVAL:
-            eval_hook_lt = []
-
-            def metric_fn(per_example_loss, label, logits):
-                predictions = tf.argmax(logits, axis=-1, output_type=tf.int32)
-                accuracy = tf.metrics.accuracy(
-                    labels=label, predictions=predictions, weights=None)
-                loss = tf.metrics.mean(values=per_example_loss, weights=None)
-                return {
-                    "eval_accuracy": accuracy,
-                    # "eval_loss": loss,
-                }
-
-            if configDir["beholder"]:
-                activate_lt = []
-                for name, v in activate_dict.items():
-                    activate_lt.append(v)
-                beholder_hook = BeholderHook(configDir["model_dir"], [picture] + activate_lt, np.ndarray([32, 32, 3]))
-                eval_hook_lt.append(beholder_hook)
-            pic_summary = tf.summary.image("val_picture_org", picture)
-            per_example_loss_summary = tf.summary.histogram("val_per_example_loss", per_example_loss)
-            per_example_probility_summary = tf.summary.histogram("val_per_example_pribility", per_example_probility)
-            val_summary_lt.append(pic_summary)
-            val_summary_lt.append(per_example_probility_summary)
-            val_summary_lt.append(per_example_loss_summary)
-            eval_hook_lt.append(tf.train.SummarySaverHook(save_steps=configDir["save_summary_steps"],
-                                                          output_dir=configDir["model_dir"]
-                                                          , summary_op=val_summary_lt))
-            output_spec = tf.estimator.EstimatorSpec(
-                mode=mode,
-                loss=-tf.reduce_mean(per_example_loss, axis=-1),
-                eval_metric_ops=metric_fn(per_example_loss, label, cls),
-                scaffold=scaffold_fn,
-                evaluation_hooks=eval_hook_lt
-            )
-
-        elif mode == tf.estimator.ModeKeys.PREDICT:
-            predictions = {
-                "unique_ids": unids,
-                "predict": tf.arg_max(cls, -1),
-                # "logits": tf.arg_max(tf.nn.softmax(cls, -1), -1),
-                "label": label,
-                "category_probility": tf.nn.softmax(cls,-1),
-                "path": features["name"]
-            }
-            if configDir["do_save_conv_image"]:
-                for k,v in activate_dict.items():
-                    predictions[k] = v
-
-            if configDir["grad_cam"]:
-                def grad_cam(conv_layer,loss):
-                    grads = tf.gradients(loss,conv_layer)[0]
-                    norm_grads = tf.nn.l2_normalize(grads,axis=[1,2,3])
-                    if model_config["data_format"] == "channels_first":
-                        axis = [2,3]
-                        axis1 = 1
-                    else:
-                        axis = [1,2]
-                        axis1 = -1
-                    weight = tf.reduce_mean(norm_grads,axis=axis,keepdims=True)
-                    cams = conv_layer * weight
-                    cams = tf.reduce_sum(cams,axis1,keepdims=False)
-                    cams = tf.nn.relu(cams)
-                    return cams
-
-                conv_layer = activate_dict[configDir["layer_name"]]
-                cam = grad_cam(conv_layer,per_example_probility)
-                predictions["cam"] = cam
-                predictions["image"] = organ_image
-
-            output_spec = tf.estimator.EstimatorSpec(
-                mode=mode, predictions=predictions, scaffold=scaffold_fn)
-        else:
-            raise ValueError(
-                "Only TRAIN and PREDICT modes are supported: %s" % (mode))
-
-        return output_spec
-
-    return model_fn
-
+def filter_conv_image(predict_dic):
+    image = {}
+    for k, v in predict_dic.items():
+        if k.find("conv") != -1 or k.find("block") != -1:
+            image[k] = v
+    return image
 
 def main(_):
     tf.logging.set_verbosity(tf.logging.INFO)
@@ -342,7 +62,7 @@ def main(_):
             train_examples / configDir["train_batch_size"] * configDir["num_train_epochs"])
         num_warmup_steps = int(num_train_steps * configDir["warmup_proportion"])
 
-    model_fn = model_fn_builder(
+    model_fn = model_build.model_fn_builder(config=configDir,model_config=model_config,
             learning_rate=configDir["learning_rate"], num_train_steps=num_train_steps,
                                 num_warmup_steps=num_warmup_steps)
 
@@ -376,11 +96,11 @@ def main(_):
             elif configDir["tfdbgtensorboard"]:
                 trainHookLt.append(tfdbg.TensorBoardDebugHook(grpc_debug_server_addresses="localhost:11111"))
 
-        train_input_fn = file_based_input_fn_builder(input_file=configDir["train_input"], is_training=True, drop_remainder=True,
+        train_input_fn = model_input.file_based_input_fn_builder(input_file=configDir["train_input"], is_training=True, drop_remainder=True,
                                                      batch="train_batch_size")
 
         # input_files = os.listdir(os.path.join(configDir["DP"], "test"))
-        val_input_fn = file_based_input_fn_builder(input_file=configDir["val_input"], is_training=False, drop_remainder=True,
+        val_input_fn = model_input.file_based_input_fn_builder(input_file=configDir["val_input"], is_training=False, drop_remainder=True,
                                                    batch="eval_batch_size")
         trainSpec = tf.estimator.TrainSpec(input_fn=train_input_fn, max_steps=num_train_steps, hooks=trainHookLt)
         valSpec = tf.estimator.EvalSpec(input_fn=val_input_fn, steps=configDir["trainStepVal"],
@@ -388,18 +108,12 @@ def main(_):
         tf.estimator.train_and_evaluate(estimator=estimator, train_spec=trainSpec, eval_spec=valSpec)
 
     if configDir["do_test"]:
-        def filter_conv_image(predict_dic):
-            image = {}
-            for k,v in predict_dic.items():
-                if k.find("conv") != -1 or k.find("block") != -1:
-                    image[k] = v
-            return image
 
         tf.logging.info("***** Running predictions *****")
         tf.logging.info("  Batch size = %d", configDir["test_batch_size"])
 
         # input_files = os.listdir(os.path.join(configDir["DP"], "test"))
-        predict_input_fn = file_based_input_fn_builder(input_file=configDir["predict_input"], is_training=False, drop_remainder=True,
+        predict_input_fn = model_input.file_based_input_fn_builder(input_file=configDir["predict_input"], is_training=False, drop_remainder=True,
                                                        batch="predict_batch_size")
 
         wf = open(configDir["test_res_output"], "w", encoding="utf-8")
@@ -429,14 +143,6 @@ def main(_):
                 numpy_path = os.path.join(configDir["conv_image_path"],os.path.basename(path)[:-4])
                 np.savez(numpy_path,**conv_image)
             if configDir["grad_cam"]:
-                def show_cam_on_image(img, mask):
-                    heatmap = cv2.applyColorMap(np.uint8(255 * mask), cv2.COLORMAP_JET)
-                    heatmap = np.float32(heatmap) / 255
-                    cam_out = heatmap + np.float32(img)
-                    cam_out = cam_out / np.max(cam_out)
-                    plt.imshow(cam_out)
-                    plt.show()
-
                 cam = result["cam"]
                 image = result["image"]
 
---
+++ utility/data_tool/from_txt_get_data.py
@@ -0,0 +1,206 @@
+import re
+import json
+import numpy as np
+import os
+import collections
+import random
+
+
+class souhu_data():
+    def __init__(self, data, istrain=True,max_query_length=-1):
+        paten = re.compile(r"\n+")
+        self.newsid = data["newsId"]
+        self.title = data['title']
+        self.content = self.title + data['content'].strip() + self.title
+        self.content = re.sub(paten, " ", self.content)
+        self.entity_mon = list()
+        if istrain:
+            for dic in data['coreEntityEmotions']:
+                self.entity_mon.append((dic["entity"].strip(), dic["emotion"].strip()))
+        if max_query_length != -1 and istrain:
+            emotion2num = {"POS": 0, "NORM": 1, "NEG": 2}
+            if len(self.entity_mon) >3:
+                self.entity_mon = self.entity_mon[:3]
+            if len(self.entity_mon) <3:
+                for i in range(3-len(self.entity_mon)):
+                    self.entity_mon.append(self.entity_mon[random.randint(0,len(self.entity_mon)-1)])
+            self.core = [i[0] for i in self.entity_mon]
+            self.emotion = [i[1] for i in self.entity_mon]
+            self.emoiton = [emotion2num[emo] for emo in self.emotion]
+            self.core_mark = self._get_predict_label_mark(max_query_length)
+
+
+    def _get_predict_label_mark(self, max_query_length):
+        label = np.zeros(shape=(3, max_query_length))
+        end = -1
+        for i, res_i in enumerate(self.core):
+            if i >= 3:
+                break
+            start = end + 1  # end 指向@符号
+            end = start + len(res_i)
+            if end > max_query_length:
+                break
+            for j in range(start, end):
+                label[i][j] = 1
+        return label
+
+    def UpdateCoreAndCoremark(self,core,max_query_length):
+        if len(core) > 3:
+            core = core[:3]
+        if len(core) < 3:
+            for i in range(3 - len(core)):
+                core.append(core[random.randint(0, len(core) - 1)])
+        self.core = core
+        self.core_mark = self._get_predict_label_mark(max_query_length)
+        self.emotion = None
+
+class sub_res_data():
+    def __init__(self, line, istrain, max_query_length):
+        temp = line.strip().split("\t")
+        self.new_id = temp[0]
+        self.predict_res = temp[1].split("*|||*")
+        try:
+            self.res_index = [[int(j) for j in i.split(",")] for i in temp[4].split("*|||*")]
+        except Exception as e:
+            print(e)
+        self.len_paragraph = int(temp[5].split("*|||*")[0])
+        self.score = [float(i) for i in temp[3].split("*|||*")]
+        self.vote_score = temp[2].split("*|||*")
+        if istrain:
+            self.res = temp[6].split("*|||*")
+            self.res_point, self.tag = self._get_res_point()
+            self.res_point = self._get_one_hot_labels()
+        self.res_mark = self._get_predict_label_mark(max_query_length)
+
+    def _get_res_point(self):
+        res_lt = []
+        tag = "good_data"
+        for res in self.res:
+            try:
+                res_lt.append(self.predict_res.index(res))
+            except ValueError as e:
+                continue
+        if len(res_lt) == 0:
+            tag = "bad_data"
+        elif max(res_lt) > 450: #todo:这里写的不对，后期改为检测文本长度
+            tag = "bad_data"
+        return res_lt, tag
+
+    def _add_right_res(self):
+        pass
+
+    def _get_one_hot_labels(self):
+        label = np.zeros(shape=(40))
+        for i in range(20):
+            if i in self.res_point:
+                label[2 * i] = 1
+            else:
+                label[2 * i + 1] = 1
+        return label
+
+    def _get_predict_label_mark(self, max_query_length):
+        label = np.zeros(shape=(20, max_query_length))
+        end = -1
+        for i, res_i in enumerate(self.predict_res):
+            start = end + 1  # end 指向@符号
+            end = start + len(res_i)
+            if end > max_query_length:
+                break
+            for j in range(start, end):
+                label[i][j] = 1
+        return label
+
+
+class sub_result():
+    def __init__(self, new_id, res_prediction):
+        self.new_id = new_id
+        self.res_prediction = res_prediction
+
+
+class train_data():
+    def __init__(self, new_id, content, predict, label=None, label_mark=None):
+        self.new_id = new_id
+        self.content = content
+        self.predict = predict
+        self.label = label
+        self.label_mark = label_mark
+
+
+
+class data_util():
+    def __init__(self):
+        self.souhu_data = {}
+        self.sub_data = []
+        self.train_data = []
+
+    def get_souhu_data(self, file_name, istrain,max_query_length=-1):
+        for line in open(file_name, "r", encoding="utf-8"):
+            data = souhu_data(json.loads(line.strip()), istrain,max_query_length)
+            self.souhu_data[data.newsid] = data
+
+    def get_sub_res_data(self, file_name, istrain, max_query_length):
+        for line in open(file_name, "r", encoding="utf-8"):
+            data = sub_res_data(line.strip(), istrain, max_query_length)
+            if istrain:
+                if data.tag == "good_data":
+                    self.sub_data.append(data)
+            else:
+                self.sub_data.append(data)
+
+    def get_sub_train_data(self):
+        assert len(self.souhu_data) != 0
+        assert len(self.sub_data) != 0
+        for sub_data in self.sub_data:
+            self.train_data.append(
+                train_data(sub_data.new_id, self.souhu_data[sub_data.new_id].content, sub_data.predict_res,
+                           sub_data.res_point, sub_data.res_mark))
+
+    def get_sub_prediction_data(self):
+        assert len(self.souhu_data) != 0
+        assert len(self.sub_data) != 0
+        for sub_data in self.sub_data:
+            self.train_data.append(
+                train_data(sub_data.new_id, self.souhu_data[sub_data.new_id].content, sub_data.predict_res,
+                           label_mark=sub_data.res_mark))
+
+
+
+
+class Tool():
+    @staticmethod
+    def merge_txt(file_base, output_file):
+        wf = open(output_file, "w", encoding="utf-8")
+        file_names = os.listdir(file_base)
+        for file_name in file_names:
+            file_name = os.path.join(file_base, file_name)
+            with open(file_name, "r", encoding="utf-8") as rf:
+                for line in rf:
+                    if line == "\n": continue
+                    wf.write(line)
+        wf.close()
+
+    @staticmethod
+    def split_txt(input_file, output_file_base, len_file):
+        if not os.path.exists(output_file_base):
+            os.mkdir(output_file_base)
+        input_file_name = os.path.basename(input_file)
+        with open(input_file, "r", encoding="utf-8") as rf:
+            i = 1
+            for index, line in enumerate(rf):
+                # print(index)
+                if index % len_file == 0:
+                    if i != 1:
+                        wf.close()
+                    wf = open("{}{}.txt".format(os.path.join(output_file_base, input_file_name.split(".")[0]), i), "w",
+                              encoding="utf-8")
+                    i += 1
+                wf.write(line)
+            wf.close()
+    @staticmethod
+    def get_all_core(all_core_file):
+        all_core = {}
+        with open(all_core_file, "r", encoding="utf-8") as rf:
+            for line in rf:
+                temp = line.strip().split("\t")
+                all_core[temp[0]] = temp[1].split("*|||*")
+        return all_core
